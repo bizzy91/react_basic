c91941ba0004f0b49dabd02eb49e39a5a9fdcaa1
Byeong-gyu Park <qurious@Byeong-gyus-MacBook-Air.local>
2022-05-02 15:58:44 +0900

    First Component

---
+++ react_test/src/Hello.js
@@ -0,0 +1,11 @@
+// React 모듈 불러오기
+import React from 'react';
+
+
+// Hello 라는 이름의 컴포넌트 생성하기
+function Hello() {
+  return <div>First Component</div>
+}
+
+// Hello 를 컴포넌트로 내보내기
+export default Hello;
